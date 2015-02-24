1f21e96cf46a45b55be598a97874806f542e0041
anticapitalista <antiX@operamail.com>
2015-02-24 17:39:50 +0200

    Updated for antiX-15 release

--- antixsources.sh
+++ antixsources.sh
@@ -2,24 +2,30 @@
 # File Name: antixsources.sh
 # Purpose: script to set initial Debian repository (and antiX)
 # Author: anticapitalista for antiX
-# Latest Change: 26 December 2013
-# Added options for antiX repositories. 25 October 2013.
+# Latest Change: 23 January 2015
+# Added options for up-coming Jessie repositories. 17 December 2014.
+# Set default to Jessie.
 ##################################################################################
 
 TEXTDOMAINDIR=/usr/share/locale 
-TEXTDOMAIN=antixsources.sh
+TEXTDOMAIN=antixsources
 
 export title="antixsources"
 
-info_text=$"This <b>$title</b> tool sets your default Debian and antiX repository and then runs the installer.\n\\n\
-You can choose between: \n\\n\
-<b>Wheezy/Stable</b>\n\\n\
-<b>Testing</b> or \n\\n\
-<b>Sid/Unstable</b>.\n\\n\
-This release of antiX defaults to <b>Testing</b> and <b>antiX/Testing</b>.\n\\n\
-If you do not know what to do, choose <b>Cancel</b> and the \n\
-<b>Testing</b> repositories will be used and the installer will begin.\n\\n\
-Press <b>OK</b> to continue."
+info_text=$"This tool sets your default Debian and antiX repository and then runs the installer.
+
+You can choose between:
+
+Jessie/Stable
+Testing
+Sid/Unstable
+
+This release of antiX defaults to Jessie/Stable and antiX/Stable.
+
+If you do not know what to do, choose Cancel and the 
+Jessie/Stable repositories will be used and the installer will begin.
+
+Press OK to continue."
 
 # width of progress dialogs
 WIDTH=300
@@ -27,24 +33,24 @@ WIDTH=300
 # edit Debian sources.list
 function edit_sources
 {
-    # Wheezy
-    if [ "$wheezy" = true ]; then
-        sed -i -r '/wheezy/ s/^#+//' /etc/apt/sources.list.d/debian.list 
-        sed -i -r '/http:.*multimedia\.org.* testing/ s/^([^#])/#\1/' /etc/apt/sources.list.d/debian.list    
-        sed -i -r '/testing/ s/^([^#])/#\1/' /etc/apt/sources.list.d/debian.list        
-        sed -i -r '/deb-src/ s/^([^#])/#\1/' /etc/apt/sources.list.d/debian.list
-        sed -i -r '/stable/ s/^#+//' /etc/apt/sources.list.d/antix.list 
-        sed -i -r '/testing/ s/^/#/' /etc/apt/sources.list.d/antix.list
+    # Jessie
+    if [ "$jessie" = true ]; then
+        echo "do nothing"
     fi
     # Testing
     if [ "$testing" = true ]; then
-        echo "do nothing"
+        sed -i -r '/testing/ s/^#+//' /etc/apt/sources.list.d/debian.list 
+        sed -i -r '/http:.*multimedia\.org.* testing/ s/^([^#])/#\1/' /etc/apt/sources.list.d/debian.list    
+        sed -i -r '/jessie/ s/^([^#])/#\1/' /etc/apt/sources.list.d/debian.list        
+        sed -i -r '/deb-src/ s/^([^#])/#\1/' /etc/apt/sources.list.d/debian.list
+        sed -i -r '/testing/ s/^#+//' /etc/apt/sources.list.d/antix.list 
+        sed -i -r '/stable/ s/^/#/' /etc/apt/sources.list.d/antix.list
     fi
     # Sid
     if [ "$sid" = true ]; then
         sed -i -r '/unstable/ s/^#+//' /etc/apt/sources.list.d/debian.list
         sed -i -r '/http:.*multimedia\.org.* unstable/ s/^([^#])/#\1/' /etc/apt/sources.list.d/debian.list    
-        sed -i -r '/wheezy/ s/^([^#])/#\1/' /etc/apt/sources.list.d/debian.list 
+        sed -i -r '/jessie/ s/^([^#])/#\1/' /etc/apt/sources.list.d/debian.list 
         sed -i -r '/testing/ s/^([^#])/#\1/' /etc/apt/sources.list.d/debian.list  
         sed -i -r '/deb-src/ s/^([^#])/#\1/' /etc/apt/sources.list.d/debian.list
         sed -i -r '/testing/ s/^#+//' /etc/apt/sources.list.d/antix.list 
@@ -55,7 +61,7 @@ function edit_sources
 function success
 {
 	# tell user 
-	yad --image "info" --center --title "$title" --text=$"Your Debian and antiX repositories are set to <b>$x</b>"
+	yad --image "info" --center --title "$title" --text=$"Your Debian and antiX repositories are set to $x"
 }
 
 function minstall
@@ -83,13 +89,13 @@ if [ $rsp != 0 ]; then
 fi
 
 # selection dialog
-ans=$(yad --title "$title" \
+ans=$(yad --center --title "$title" \
              --width "$WIDTH" --height 220 \
              --list --separator=":" \
-             --text $"Choose <b>ONE</b> repository for Debian and antiX" \
-             --checklist  --column $"Choose" --column $"Repository"\
-             FALSE "Wheezy" \
-              TRUE "Testing" \
+             --text $"Choose ONE repository for Debian and antiX" \
+             --radiolist  --column $"Choose" --column $"Repository"\
+              TRUE "Jessie" \
+             FALSE "Testing" \
              FALSE "Sid")
 
 #echo $ans
@@ -102,8 +108,8 @@ for x in $arr
 do
     #echo "> [$x]"
     case $x in
-    Wheezy)
-        wheezy='true'
+    Jessie)
+        jessie='true'
         selected='yes'
         ;;
     Testing)
